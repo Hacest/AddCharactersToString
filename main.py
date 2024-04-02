6c23ee18f773030670cfee9ed703ae8ded331910
Hacest <haisoo08@gmail.com>
2024-04-02 14:21:39 +0000

    Code snippet for adding characters to values

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ list_string = list.split('\n')
 
 for id in list_string:
   final_list.append(
-    "<아이템 생성 치트> " + id + ""
+    "<Character to add> " + id + " <Character to add>"
   )
 
 for id in final_list:
